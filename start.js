5f45045581112fd7c7ebc37741a3ab1080495b7f
Dhanushka dilshan <dnkdilshan@gmail.com>
2025-05-14 11:22:55 +0530

    code format

--- start.js
+++ start.js
@@ -1,15 +1,15 @@
-const setupEvents = require('./installers/setupEvents')
- if (setupEvents.handleSquirrelEvent()) {
-    return;
- }
+const setupEvents = require("./installers/setupEvents");
+if (setupEvents.handleSquirrelEvent()) {
+  return;
+}
 
-const server = require('./server');
-const {app, BrowserWindow, ipcMain, screen} = require('electron');
-const path = require('path')
+const server = require("./server");
+const { app, BrowserWindow, ipcMain, screen } = require("electron");
+const path = require("path");
 
-const contextMenu = require('electron-context-menu');
+const contextMenu = require("electron-context-menu");
 
-let mainWindow
+let mainWindow;
 
 function createWindow() {
   var primaryDisplay = screen.getPrimaryDisplay();
@@ -18,75 +18,66 @@ function createWindow() {
     width: screenDimensions.width,
     height: screenDimensions.height,
     frame: false,
-    minWidth: 1200, 
+    minWidth: 1200,
     minHeight: 750,
-    
+
     webPreferences: {
       nodeIntegration: true,
       enableRemoteModule: true,
-      contextIsolation: false
+      contextIsolation: false,
     },
   });
 
   mainWindow.maximize();
   mainWindow.show();
 
-  mainWindow.loadURL(
-    `file://${path.join(__dirname, 'index.html')}`
-  )
+  mainWindow.loadURL(`file://${path.join(__dirname, "index.html")}`);
 
-  mainWindow.on('closed', () => {
-    mainWindow = null
-  })
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
 }
 
+app.on("ready", createWindow);
 
-app.on('ready', createWindow)
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-})
+});
 
-app.on('activate', () => {
+app.on("activate", () => {
   if (mainWindow === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
 
+ipcMain.on("app-quit", (evt, arg) => {
+  app.quit();
+});
 
-
-ipcMain.on('app-quit', (evt, arg) => {
-  app.quit()
-})
-
-
-ipcMain.on('app-reload', (event, arg) => {
+ipcMain.on("app-reload", (event, arg) => {
   mainWindow.reload();
 });
 
-
-
 contextMenu({
   prepend: (params, browserWindow) => [
-     
-      {label: 'DevTools',
-       click(item, focusedWindow){
+    {
+      label: "DevTools",
+      click(item, focusedWindow) {
         focusedWindow.toggleDevTools();
-      }
+      },
     },
-     { 
-      label: "Reload", 
-        click() {
-          mainWindow.reload();
-      } 
-    // },
-    // {  label: 'Quit',  click:  function(){
-    //    mainWindow.destroy();
-    //     mainWindow.quit();
-    // } 
-  }  
+    {
+      label: "Reload",
+      click() {
+        mainWindow.reload();
+      },
+      // },
+      // {  label: 'Quit',  click:  function(){
+      //    mainWindow.destroy();
+      //     mainWindow.quit();
+      // }
+    },
   ],
-
 });
